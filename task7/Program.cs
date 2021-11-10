3727c501c146d4adc485f696c72edb04e86c3a8f
antonMark <anton_markin98@bk.ru>
2021-11-10 20:35:47 +0300

    task7

---
+++ task7/Program.cs
@@ -0,0 +1,15 @@
+﻿// 7. Показать числа от -N до N
+
+int [] array = {-5,-4,-3,-2,-1,0,1,2,3,4,5};
+int i =0; 
+while (i<11)
+{
+     Console.WriteLine(array[i]);
+     i++;
+}
+Console.WriteLine(array[i]);
+
+
+
+
+
