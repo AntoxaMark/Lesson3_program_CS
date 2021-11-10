bc320dabc91c5287e0b72bd41aa89a5bec5f8852
antonMark <anton_markin98@bk.ru>
2021-11-10 21:43:31 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,14 @@
+﻿// 27. Определить количество цифр в числе
+
+int CountDigit (int n)
+{
+    int k=0;
+    while (n!=0)
+    {
+        k++; // k=k=1;
+        n=n/10;
+    }
+    return k;
+}
+
+Console.WriteLine(CountDigit(12345));
