e14598ddce1c542464c2e8fb04be894ee98f847f
antonMark <anton_markin98@bk.ru>
2021-11-10 21:34:34 +0300

    Task 22

---
+++ task22/Program.cs
@@ -0,0 +1,9 @@
+﻿// 22. Найти расстояние между точками в пространстве 2D/3D
+
+double Distance2D (double x1, double y1, double x2, double y2)
+{
+    return Math.Sqrt(Math.Pow(x2-x1,2))+Math.Pow(y2-y1,2);
+
+}
+Console.WriteLine(Distance2D (0,0,2,2));
+Console.WriteLine();
