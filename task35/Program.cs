f842b0effebade4bd900b5e228442cf36ee22699
antonMark <anton_markin98@bk.ru>
2021-11-10 22:30:47 +0300

    task35

---
+++ task35/Program.cs
@@ -0,0 +1,21 @@
+﻿// 35. Определить, присутствует ли в заданном массиве, некоторое число 
+
+int [] array = new int [20];
+int ThisNumbers;
+
+for (int index=0;index<array.Length;index++)
+{
+    array[index] = new Random().Next(1,99);
+    Console.WriteLine(array[index]);
+
+}
+Console.WriteLine("Введите Число");
+ThisNumbers = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]  == ThisNumbers)
+    Console.WriteLine("В массиве присутсвует" +ThisNumbers);
+    else 
+    Console.WriteLine("Числа в массие нет");
+}
+
