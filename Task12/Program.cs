0a01bf1385e760646fdfeb92851f79e2cf804849
antonMark <anton_markin98@bk.ru>
2021-11-11 19:12:50 +0300

    task12

---
+++ Task12/Program.cs
@@ -0,0 +1,8 @@
+﻿//Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите техзначное число");
+int b, a;
+a = Convert.ToInt32(Console.ReadLine());
+
+b = a / 100 * 10 + a % 10;
+Console.WriteLine("Число  " + b);
