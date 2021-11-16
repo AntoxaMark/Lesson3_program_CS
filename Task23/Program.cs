96178d72afe6139a0bdcc2fc4d316806cdecc9a6
antonMark <anton_markin98@bk.ru>
2021-11-16 22:54:34 +0300

    task23

---
+++ Task23/Program.cs
@@ -0,0 +1,17 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N
+
+
+
+for (int i = 1; i<2; i++)
+{
+    {
+        for (int m = 10; m < 100; m++)
+        {
+            int result = 0;
+            result = m*m;
+            Console.WriteLine($" {m}* {m} = {m*m} " );
+        }
+    }
+    Console.WriteLine();
+    
+} 
