d8e3eba6444b323506b1d1bc7cc7a0266b29c108
antonMark <anton_markin98@bk.ru>
2021-11-17 22:01:41 +0300

    task20

---
+++ task20/Program.cs
@@ -0,0 +1,12 @@
+﻿// 20. Ввести номер четверти, показать диапазоны для возможных координат
+
+int a;
+
+Console.WriteLine("Введите четверть");
+a = Convert.ToInt32(Console.ReadLine());
+if (a == 1 )  Console.WriteLine ("Диапозон y=[0;+N] x=[0;+N]");
+if (a == 2 )  Console.WriteLine ("Диапозон y=[0;+N] x=[0;-N]");
+if (a == 3 )  Console.WriteLine ("Диапозон y=[0;-N] x=[0;-N]");
+if (a == 4 )  Console.WriteLine ("Диапозон y=[0;-N] x=[0;+N]");
+
+
